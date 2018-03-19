249c3c97cf14375c0c511fd9ea4338677497303d
Kaleb McKone <kaleb.mckone@thomsonreuters.com>
2018-03-19 15:07:08 -0500

    Added .sh to bash scripts

---
+++ testScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "$0 is the name of this script"
+
+echo "$1 is the first command line argument"
+echo "$2 is the second command line argument"
+
+VAR1='value for var 1'
+VAR2='value for var 2'
+
+echo "$VAR1 is the value for var 1"
+echo "$VAR2 is the value for var 2"
+
